b20825e2b8cf311642bd9062bee436382632398c
pjohans <pjohans@gmail.com>
2016-08-17 09:25:39 +0200

    add empty interface ITingClientRequestCache for backwards compatibility + made default cache an object

---
+++ lib/cache/ITingClientRequestCache.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * @file
+ *
+ * ***********************************
+ * DUMMY FILE
+ * this is an empty interface for backwards compatibility with the old ting-client
+ *
+ *
+ * Class ITingClientRequestCache
+ */
+
+interface ITingClientRequestCache{
+
+}
