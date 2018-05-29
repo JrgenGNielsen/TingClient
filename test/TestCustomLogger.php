190bc8f5c6cd479461efdf917bf502161f5c6c9a
pjohans <pjohans@gmail.com>
2018-05-29 13:32:37 +0200

    updated to new interface

--- test/TestCustomLogger.php
+++ test/TestCustomLogger.php
@@ -1,6 +1,6 @@
 <?php
 class TestCustomLogger extends TingClientLogger {
-  protected function doLog($message, $variables, $severity) {
+  protected function doLog($message, $variables, $severity, $raw_entry) {
     print 'LOGGING';
   }
 }
